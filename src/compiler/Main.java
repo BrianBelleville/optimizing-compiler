6a97cb382ecea4f9895564030a908f8877cdd0f9
Brian Belleville <brian.belleville@uci.edu>
2014-01-24 14:58:35 -0800

    Add Main which has main method of the compiler

---
+++ src/compiler/Main.java
@@ -0,0 +1,16 @@
+package compiler;
+
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            File f = new File(args[0]);
+            Parser parse = new Parser(f);
+            parse.parse();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
