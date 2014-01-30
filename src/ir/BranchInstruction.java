285f1b2db4a49d30713a1deabea085e8b9f557d6
Brian Belleville <brian.belleville@uci.edu>
2014-01-30 15:38:08 -0800

    Add class for branch instructions

---
+++ src/ir/BranchInstruction.java
@@ -0,0 +1,43 @@
+package ir;
+
+import java.util.ArrayList;
+
+public class BranchInstruction extends Instruction {
+    private BasicBlock target;
+
+    public BranchInstruction(int num, BasicBlock containing, Opcode o, BasicBlock target) {
+	super(num, containing, o);
+	this.target = target;
+    }
+
+    public BasicBlock getTarget() {
+	return target;
+    }
+
+    @Override
+    public void replaceArgument(Instruction replace, Instruction i)
+	throws Exception {
+	throw new Exception("Error, attempt to replace arguments of a branch instruction");
+    }
+
+    @Override
+    public boolean isCommonSubexpression(Instruction i) {
+	return false;		// branches can never be common subexpressions
+    }
+
+    @Override
+    public void performCSE() {
+	return;			// no op, branches can never be common subexpressions
+    }
+
+    @Override
+    public void delete(Instruction i) throws Exception {
+	throw new Exception("Error, attempt to delete a branch instruction");
+    }
+
+    @Override
+    public ArrayList<Instruction> getArguments() {
+	return null;		// branch instructions real argument is the target
+    }
+
+}
