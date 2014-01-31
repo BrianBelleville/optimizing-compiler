3a32c88a5ea8ab39eef716848780cc3a56850d1d
Brian Belleville <brian.belleville@uci.edu>
2014-01-31 15:47:31 -0800

    Add function name to call instruction

--- src/ir/instructions/Call.java
+++ src/ir/instructions/Call.java
@@ -6,10 +6,12 @@ import ir.BasicBlock;
 import ir.Instruction;
 import ir.NaryInstruction;
 import ir.Opcode;
+import support.Identifier;
 
 public class Call extends NaryInstruction {
-
-    public Call(BasicBlock b,  ArrayList<Instruction> args) {
+    private Identifier functionName;
+    public Call(BasicBlock b,  Identifier function, ArrayList<Instruction> args) {
         super(b, Opcode.call, args);
+	functionName = function;
     }
 }
