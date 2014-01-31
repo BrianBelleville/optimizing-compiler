4157a89d5a15658dc0f3ec98b66be3de1def6349
Brian Belleville <brian.belleville@uci.edu>
2014-01-31 15:48:37 -0800

    Add variable name to store instruction

--- src/ir/instructions/Store.java
+++ src/ir/instructions/Store.java
@@ -4,11 +4,16 @@ import ir.BasicBlock;
 import ir.BinaryInstruction;
 import ir.Instruction;
 import ir.Opcode;
+import support.Identifier;
 
 public class Store extends BinaryInstruction {
-
-    public Store(BasicBlock b, Instruction a1, Instruction a2) {
-        super(b, Opcode.store, a1, a2);
+    private Identifier variable;
+    public Store(BasicBlock b, Identifier var, Instruction a1, Instruction a2) {
+	super(b, Opcode.store, a1, a2);
+	variable = var;
     }
 
+    public Identifier getVariable() {
+	return variable;
+    }
 }
