0f0c2a5f7631f49b06869839d57795b92382ceb1
Brian Belleville <brian.belleville@uci.edu>
2014-01-31 14:17:59 -0800

    Add a variable field to Phi instructions
    This will provide the semantic information necessary to see what
    identifier the phi is choosing values for.

--- src/ir/instructions/Phi.java
+++ src/ir/instructions/Phi.java
@@ -4,11 +4,16 @@ import ir.BasicBlock;
 import ir.BinaryInstruction;
 import ir.Instruction;
 import ir.Opcode;
+import support.Identifier;
 
 public class Phi extends BinaryInstruction {
-
-    public Phi(BasicBlock b, Instruction a1, Instruction a2) {
+    private Identifier variable;
+    public Phi(BasicBlock b, Identifier var, Instruction a1, Instruction a2) {
         super(b, Opcode.phi, a1, a2);
+	variable = var;
     }
 
+    public Identifier getVariable() {
+	return variable;
+    }
 }
