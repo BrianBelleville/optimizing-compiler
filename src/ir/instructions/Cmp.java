19ca46a64701c0e65fe7372695436b501ff000d2
Brian Belleville <brian.belleville@uci.edu>
2014-01-31 16:25:45 -0800

    Add semantic information to Cmp instruction

--- src/ir/instructions/Cmp.java
+++ src/ir/instructions/Cmp.java
@@ -5,9 +5,21 @@ import ir.Instruction;
 import ir.Opcode;
 
 public class Cmp extends BinaryInstruction {
-
-    public Cmp(Instruction a1, Instruction a2) {
+    public enum CmpType {
+	eq,
+	neq,
+	lt,
+	lte,
+	gt,
+	gte
+    }
+    private CmpType cmpType;
+    public Cmp(CmpType type, Instruction a1, Instruction a2) {
         super(Opcode.cmp, a1, a2);
+	cmpType = type;
     }
 
+    public CmpType getType() {
+	return cmpType;
+    }
 }
