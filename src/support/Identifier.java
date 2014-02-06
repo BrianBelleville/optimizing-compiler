438ea8f3696574bea58513db6007df63f2aa82fa
Brian Belleville <brian.belleville@uci.edu>
2014-02-06 12:29:47 -0800

    Implement object.hashCode() for Identifier

--- src/support/Identifier.java
+++ src/support/Identifier.java
@@ -5,23 +5,28 @@ public class Identifier {
     private IdentifierTable table;
 
     public Identifier(int index, IdentifierTable table) {
-	table_index = index;
-	this.table = table;
+        table_index = index;
+        this.table = table;
     }
 
     public String getString() {
-	return table.getString(table_index);
+        return table.getString(table_index);
     }
 
     public boolean equals(Identifier i) {
-	return table == i.table && table_index == i.table_index;
+        return table == i.table && table_index == i.table_index;
     }
 
     @Override
     public boolean equals(Object o) {
-	if(o instanceof Identifier) {
-	    return this.equals((Identifier)o);
-	}
-	return false;
-    }    
+        if(o instanceof Identifier) {
+            return this.equals((Identifier)o);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return new Integer(table_index).hashCode();
+    }
 }
