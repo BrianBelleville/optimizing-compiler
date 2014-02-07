d40285b65b19995fa683007bccdb3cf99fbd3a34
Brian Belleville <brian.belleville@uci.edu>
2014-02-07 13:21:48 -0800

    Initialize all fields of IdentifierTable

--- src/support/IdentifierTable.java
+++ src/support/IdentifierTable.java
@@ -4,16 +4,19 @@ import java.util.ArrayList;
 
 public class IdentifierTable {
     private ArrayList<String> string_tab;
+    public IdentifierTable() {
+        string_tab = new ArrayList<String>();
+    }
 
     public String getString(int table_index) {
-	return string_tab.get(table_index);
+        return string_tab.get(table_index);
     }
     public Identifier addToTable(String s) {
-	int index = string_tab.indexOf(s);
-	if(index == -1) {
-	    index = string_tab.size();
-	    string_tab.add(s);
-	}
-	return new Identifier(index, this);
+        int index = string_tab.indexOf(s);
+        if(index == -1) {
+            index = string_tab.size();
+            string_tab.add(s);
+        }
+        return new Identifier(index, this);
     }
 }
