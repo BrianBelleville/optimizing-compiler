5241a175fb35686654b9ef78b308afa8e27f28d3
Brian Belleville <brian.belleville@uci.edu>
2014-02-13 23:40:50 -0800

    Implement CSE for load instructions
    This is maybe a little more conservative than it needs to be, it only
    operates within the same basic block. This is because there could be
    stores in a basic block that is not dominating, but does come before the
    containing basic block in the CFG.

--- src/ir/Load.java
+++ src/ir/Load.java
@@ -1,5 +1,8 @@
 package ir;
 
+import ir.Store;
+import ir.Call;
+import ir.base.Instruction;
 import ir.base.Opcode;
 import ir.base.UnaryInstruction;
 import ir.base.Value;
@@ -16,5 +19,47 @@ public class Load extends UnaryInstruction {
 
     public Identifier getVariable() {
 	return variable;
+    }
+
+    @Override
+    public boolean isCommonSubexpression(Instruction i) {
+        if(i instanceof Load) {
+            Load l = (Load)i;
+            // checks after the first are probably redundant, but are
+            // a requirement for correctness.
+            return this.getArg().equals(l.getArg())
+                && this.getVariable().equals(l.getVariable())
+                && this.global == l.global;
+        }
+        return false;
+    }
+
+    @Override
+    public void performCSE() throws Exception {
+        Instruction i = getDominating();
+        while(i != null) {
+            if(!this.getContainingBB().equals(i.getContainingBB())) {
+                // only perform CSE for loads within a basic block
+                // since there could be calls or stores in non
+                // dominating basic blocks along control flow paths to
+                // this instruction that aren't being taken into
+                // account at this point
+                return;
+            }
+            if(this.isCommonSubexpression(i)) {
+                this.delete((Instruction)i.getSubstitute());
+                return;
+            } else if (i instanceof Store) {
+                Store s = (Store)i;
+                if(this.getVariable().equals(s.getVariable())) {
+                    return;     // any store to the same variable kills the search
+                }
+            } else if (i instanceof Call && global) {
+                // global variables can be modified by function calls,
+                // so kill search on call if global
+                return;
+            }
+            i = i.getDominating();
+        }
     }
 }
