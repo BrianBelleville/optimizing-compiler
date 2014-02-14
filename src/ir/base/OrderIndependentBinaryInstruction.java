73835124be9ffcce5900021f4d49a12a924afe8b
Brian Belleville <brian.belleville@uci.edu>
2014-02-13 23:00:40 -0800

    Add OrderIndependentBinaryInstruction
    This class if for instructions where the order does not matter when
    testing if they are a common subexpression. For example addition:
    3 + 7 = 7 + 3

---
+++ src/ir/base/OrderIndependentBinaryInstruction.java
@@ -0,0 +1,22 @@
+package ir.base;
+
+public class OrderIndependentBinaryInstruction extends BinaryInstruction {
+    public OrderIndependentBinaryInstruction(Opcode o, Value a1, Value a2) {
+        super(o, a1, a2);
+    }
+    
+    @Override
+    public boolean isCommonSubexpression(Instruction i) {
+        if(i instanceof BinaryInstruction) {
+            BinaryInstruction b = (BinaryInstruction)i;
+            // argument order doesn't matter, so check both matchings
+            return this.getOpcode() == b.getOpcode()
+                && ((this.getArg1().equals(b.getArg1())
+                     && this.getArg2().equals(b.getArg2()))
+                    || (this.getArg1().equals(b.getArg2())
+                        && this.getArg2().equals(b.getArg1())));
+        }
+        return false;
+    }
+
+}
