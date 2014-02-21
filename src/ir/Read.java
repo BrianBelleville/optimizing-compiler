63180bf6f6479d92c6b370faa0ea8781e03abed5
Brian Belleville <brian.belleville@uci.edu>
2014-02-21 00:59:12 -0800

    Implement method overrides for Read

--- src/ir/Read.java
+++ src/ir/Read.java
@@ -1,5 +1,6 @@
 package ir;
 
+import ir.base.Instruction;
 import ir.base.NoArgInstruction;
 import ir.base.Opcode;
 
@@ -8,5 +9,17 @@ public class Read extends NoArgInstruction {
     public Read(){
         super(Opcode.read);
     }
+    @Override
+    public boolean isCommonSubexpression(Instruction i) {
+	return false;		// can never be common subexpressions
+    }
 
+    @Override
+    public void performCSE() {
+	return;			// no op, can never be common subexpressions
+    }
+    @Override
+    public boolean isDeadCode() {
+        return false;           // can not be dead code
+    }        
 }
