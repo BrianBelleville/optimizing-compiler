6b4c44a1a9a465b88dbd3ea64bf91e855391609a
Brian Belleville <brian.belleville@uci.edu>
2014-02-21 00:20:17 -0800

    Add ConstructUseChain

---
+++ src/transform/ConstructUseChain.java
@@ -0,0 +1,17 @@
+package transform;
+
+import java.util.ListIterator;
+import java.util.ArrayList;
+import ir.base.Value;
+import ir.base.Instruction;
+
+public class ConstructUseChain implements Pass {
+    public void run(ListIterator<Instruction> iter)
+        throws Exception {
+        Instruction i = iter.next();
+        ArrayList<Value> args = i.getArguments();
+        for(Value v : args) {
+            v.addUse(i);
+        }
+    }
+}
