71b46ad813c5a8c8a845dbb11db07e09d6a04f4a
Brian Belleville <brian.belleville@uci.edu>
2014-02-20 17:02:17 -0800

    Add CommonSubexpressionElimination pass

---
+++ src/transform/CommonSubexpressionElimination.java
@@ -0,0 +1,15 @@
+package transform;
+
+import java.util.ListIterator;
+import ir.base.Instruction;
+
+public class CommonSubexpressionElimination implements Pass {
+    public void run(ListIterator<Instruction> iter)
+    throws Exception {
+        Instruction i = iter.next();
+        i.performCSE();
+        if(i.isDeleted()) {
+            iter.remove();
+        }
+    }
+}
