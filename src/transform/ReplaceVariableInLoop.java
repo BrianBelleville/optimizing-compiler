2ece72b28eb217aea3bf463b5cb5751737c27c8e
Brian Belleville <brian.belleville@uci.edu>
2014-02-20 16:34:25 -0800

    Add pass ReplaceVariableReferenceInLoop

---
+++ src/transform/ReplaceVariableInLoop.java
@@ -0,0 +1,29 @@
+package transform;
+
+import java.util.ListIterator;
+import ir.base.Instruction;
+import ir.Phi;
+import ir.VariableReference;
+import ir.base.Value;
+import support.Identifier;
+
+public class ReplaceVariableInLoop implements Pass {
+    Phi phiInst;
+    VariableReference newVal;
+    Value oldVal;
+    public ReplaceVariableInLoop(Identifier var, Phi phi, Value oldVal) {
+        phiInst = phi;
+        newVal = new VariableReference(var, phiInst);
+        this.oldVal = oldVal;
+    }
+
+    public void run(ListIterator<Instruction> iter)
+        throws Exception {
+        Instruction i = iter.next();
+        // don't want to replace the actual phi instruction that
+        // was just created
+        if(i != phiInst) {
+            i.replaceArgument(oldVal, newVal);
+        }
+    }
+}
