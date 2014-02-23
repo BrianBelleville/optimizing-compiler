300bd1abb2a6338e23b4f57c8b928a85d1f44d38
Brian Belleville <brian.belleville@uci.edu>
2014-02-23 11:13:05 -0800

    Add ConstantArithmetic pass
    This will compute arithmetic expressions of constant values at compile
    time.

---
+++ src/transform/ConstantArithmetic.java
@@ -0,0 +1,33 @@
+package transform;
+
+import java.util.ListIterator;
+import ir.base.Instruction;
+import ir.base.ArithmeticInstruction;
+import ir.base.Value;
+
+public class ConstantArithmetic extends Pass {
+
+    @Override
+    public void run(ListIterator<Instruction> iter)
+    throws Exception {
+        Instruction i = iter.next();
+        i.getArgumentSubstitutes();
+        if(i instanceof ArithmeticInstruction) {
+            ArithmeticInstruction a = (ArithmeticInstruction) i;
+            if(a.isComputable()) {
+                // if we can perform the computation at compile time,
+                // do it and delete the instruction for the value it
+                // would produce.
+                Value v = a.performComputation();
+                a.delete(v);
+                // remove instruction if it has been computed at compile time
+                iter.remove(); 
+            }
+        }
+    }
+
+    @Override
+    public boolean requireBreadthFistTraversal() {
+        return true;
+    }
+}
