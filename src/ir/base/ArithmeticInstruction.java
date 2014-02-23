e1b356c9397e93f9316bc30490163ba4a8591295
Brian Belleville <brian.belleville@uci.edu>
2014-02-23 00:25:44 -0800

    Add arithmetic instruction type

---
+++ src/ir/base/ArithmeticInstruction.java
@@ -0,0 +1,17 @@
+package ir.base;
+
+import ir.Immediate;
+
+public class ArithmeticInstruction extends BinaryInstruction {
+    public ArithmeticInstruction(Opcode o, Value a1, Value a2) {
+        super(o, a1, a2);
+    }
+    
+    public boolean isComputable() {
+        return (getArg1() instanceof Immediate) && (getArg2() instanceof Immediate);
+    }
+    
+    public Value performComputation() throws Exception {
+        throw new Exception("performComputation not implemented");
+    }
+}
