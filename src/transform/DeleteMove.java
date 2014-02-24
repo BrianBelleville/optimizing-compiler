e6953ea06d53d3f52b22304b06c4a445f652eddd
Brian Belleville <brian.belleville@uci.edu>
2014-02-23 16:27:18 -0800

    Add DeleteMove pass to delete all move nodes

---
+++ src/transform/DeleteMove.java
@@ -0,0 +1,22 @@
+package transform;
+
+import java.util.ListIterator;
+import ir.Move;
+import ir.base.Instruction;
+
+public class DeleteMove extends Pass {
+    @Override
+    public void run(ListIterator<Instruction> iter)
+        throws Exception {
+        Instruction i = iter.next();
+        if(i instanceof Move) {
+            Move move = (Move)i;
+            // delete moves for the value that they are moving
+            move.delete(move.getArg());
+            // the move instruction will still be in the dominator
+            // tree, but will not interfere with CSE since all of the
+            // CSE methods on Move are a nop
+            iter.remove();
+        }
+    }
+}
