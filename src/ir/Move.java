f749dbed820d5c17a186cfddb99260a652ddc0c7
Brian Belleville <brian.belleville@uci.edu>
2014-02-23 16:11:08 -0800

    Add type for Move instruction
    No I am going to go back on my idea to do all copy propagation during
    parsing, and instead emit moves which will later be deleted.

---
+++ src/ir/Move.java
@@ -0,0 +1,25 @@
+package ir;
+
+import ir.base.Instruction;
+import ir.base.Opcode;
+import ir.base.UnaryInstruction;
+import ir.base.Value;
+
+public class Move extends UnaryInstruction {
+
+    public Move(Value arg) {
+        super(Opcode.move, arg);
+    }
+
+    @Override
+    public boolean isCommonSubexpression(Instruction i) {
+	return false;		// can never be common subexpressions
+    }
+
+    @Override
+    public void performCSE() {
+        // no op, can never be common subexpressions, moves should be
+        // deleted before cse anyways, but will still be traversed
+	return;
+    }
+}
