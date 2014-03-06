81571ece9b34ef893e06d6e5c4d636a8c3c90e34
Brian Belleville <brian.belleville@uci.edu>
2014-03-06 14:25:29 -0800

    Add MemoryRegion field to Function
    The memory region will manage the addresses of local arrays and spilled
    local variables.

--- src/ir/Function.java
+++ src/ir/Function.java
@@ -2,12 +2,15 @@ package ir;
 
 import support.Identifier;
 import support.InterferenceGraph;
+import support.LocalMemoryRegion;
+import support.MemoryRegion;
 import java.io.Writer;
 
 public class Function {
     public Identifier name;
     public BasicBlock entryPoint;
     public InterferenceGraph ig;
+    public MemoryRegion locals;
     public void printInterferenceGraph(Writer w) throws Exception {
         w.write("subgraph " + name.getString() + "{\n");
         ig.printGraph(w);
@@ -16,5 +19,8 @@ public class Function {
     public void printFunc(Writer w) throws Exception {
 	w.write(name.getString() + " -> " + entryPoint.getNodeName() + ";\n");
 	entryPoint.printBlock(w);
+    }
+    public Function() {
+        locals = new LocalMemoryRegion();
     }
 }
