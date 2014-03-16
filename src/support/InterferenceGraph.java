fca8b16322d0bd87ce5a10ad47b703f37cd337ec
Brian Belleville <brian.belleville@uci.edu>
2014-03-16 16:17:35 -0700

    Don't use the inner class EdgeInfo

--- src/support/InterferenceGraph.java
+++ src/support/InterferenceGraph.java
@@ -1,87 +1,82 @@
 package support;
 
 import ir.Value;
+import ir.Instruction;
+import ir.Fetch;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map.Entry;
 import java.io.Writer;
 
 public class InterferenceGraph {
-    public class EdgeInfo {
-        public HashSet<Value> edges;
-        public HashSet<Integer> forbiddenColors;
-        public EdgeInfo() {
-            edges = new HashSet<Value>();
-            forbiddenColors = new HashSet<Integer>();
-        }
-    }
-
-    private HashMap<Value, EdgeInfo> adjacencyList;
+    private HashMap<Value, HashSet<Value>> adjacencyList;
+    
     public InterferenceGraph () {
-        adjacencyList = new HashMap<Value, EdgeInfo>();
-    }
-
-    private void colorNode(Value node, Integer color) {
-        EdgeInfo e = adjacencyList.get(node);
-        assert(!e.forbiddenColors.contains(color));
-        node.setColor(color);
-        for(Value v : e.edges) {
-            adjacencyList.get(v).forbiddenColors.add(color);
-        }
+        adjacencyList = new HashMap<Value, HashSet<Value>>();
     }
 
     private Integer getAvailableColor(Value node) {
-        HashSet<Integer> forbiddenColors = adjacencyList.get(node).forbiddenColors;
+        HashSet<Integer> forbiddenColors = getForbiddenColors(node);
         // start coloring from 0, these are abstract registers, so it
         // doesn't matter that the 0 register can't be used in DLX.
-        Integer i = 0;          
+        Integer i = 0;
         while(forbiddenColors.contains(i)) {
             i++;
         }
         return i;
     }
 
-    // Very very stupid graph coloring strategy
+    private HashSet<Integer> getForbiddenColors(Value n) {
+        HashSet<Value> edges = adjacencyList.get(n);
+        HashSet<Integer> rval = new HashSet<Integer>();
+        for(Value v : edges) {
+            Integer c = v.getColor();
+            if(c != null && c != -1) {
+                rval.add(c);
+            }
+        }
+        return rval;
+    }
+
     public void colorGraph() {
         for(Value v  : adjacencyList.keySet()) {
-            colorNode(v, getAvailableColor(v));
+            v.setColor(getAvailableColor(v));
         }
     }
-                                               
 
     public void addNode(Value n) {
         if(!adjacencyList.containsKey(n)) {
-            adjacencyList.put(n, new EdgeInfo());
+            adjacencyList.put(n, new HashSet<Value>());
         }
     }
 
     public void deleteNode(Value n) {
-        HashSet<Value> edges = adjacencyList.get(n).edges;
+        HashSet<Value> edges = adjacencyList.get(n);
         adjacencyList.remove(n);
         for(Value e : edges) {
-            adjacencyList.get(e).edges.remove(n);
+            adjacencyList.get(e).remove(n);
         }
     }
 
     public void addEdge(Value v1, Value v2) {
-        EdgeInfo v1EdgeInfo = adjacencyList.get(v1);
-        EdgeInfo v2EdgeInfo = adjacencyList.get(v2);
-        if(v1EdgeInfo == null) {
-            v1EdgeInfo = new EdgeInfo();
-            adjacencyList.put(v1, v1EdgeInfo);
+        HashSet<Value> v1Edges = adjacencyList.get(v1);
+        HashSet<Value> v2Edges = adjacencyList.get(v2);
+        if(v1Edges == null) {
+            v1Edges = new HashSet<Value>();
+            adjacencyList.put(v1, v1Edges);
         }
-        if(v2EdgeInfo == null) {
-            v2EdgeInfo = new EdgeInfo();
-            adjacencyList.put(v2, v2EdgeInfo);
+        if(v2Edges == null) {
+            v2Edges = new HashSet<Value>();
+            adjacencyList.put(v2, v2Edges);
         }
-        v1EdgeInfo.edges.add(v2);
-        v2EdgeInfo.edges.add(v1);
+        v1Edges.add(v2);
+        v2Edges.add(v1);
     }
 
     public void printGraph(Writer w) throws Exception {
-            for(Entry<Value, EdgeInfo> v  : adjacencyList.entrySet()) {
+        for(Entry<Value, HashSet<Value>> v  : adjacencyList.entrySet()) {
             Value node = v.getKey();
-            HashSet<Value> edges = v.getValue().edges;
+            HashSet<Value> edges = v.getValue();
             // first write the node so that nodes that have no edges
             // connecting them will be drawn.
             w.write("\"");
